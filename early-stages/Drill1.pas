73a6210298130788d4bee89982fe33e4a750a64c
Hex <hexasweetzmain@gmail.com>
2026-01-02 20:19:32 -0500

    Delete early-stages directory

--- early-stages/Drill1.pas
+++
@@ -1,9 +0,0 @@
-{---------DRILL 1-1 --------------------}
-
-program Drill1;
-begin
-    writeln('Wordware Publishing, Inc.');
-    writeln('--------------------------');
-    writeln('2320 Los Rios Boulevard');
-    writeln('Plano, Texas 75074');
-end.
