6002d1302cac4aa8043ddd56e374b308bd4de0aa
Hex <hexasweetzmain@gmail.com>
2026-01-02 20:13:13 -0500

    Create Drill1.pas
    This MY version of Drill 1-1 in Learn Pascal in 3 days (3rd edition) by Sam A. Abolrous

---
+++ early-stages/Drill1.pas
@@ -0,0 +1,9 @@
+{---------DRILL 1-1 --------------------}
+
+program Drill1;
+begin
+    writeln('Wordware Publishing, Inc.');
+    writeln('--------------------------');
+    writeln('2320 Los Rios Boulevard');
+    writeln('Plano, Texas 75074');
+end.
