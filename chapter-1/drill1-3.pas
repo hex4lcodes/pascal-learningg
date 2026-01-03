38528c48b709549dca1655c2f67a5dc946d50be6
Hex <hexasweetzmain@gmail.com>
2026-01-02 21:36:42 -0500

    Create drill1-3.pas

---
+++ chapter-1/drill1-3.pas
@@ -0,0 +1,21 @@
+{---------DRILL 1-3 --------------------}
+
+program Drill3;
+begin
+    {----part a----}
+    writeln(15-15 div 15);
+    {14 integer}
+
+    {----part b----}
+    writeln(22+10/2);
+    {2.7000000000000000E+001 real}
+
+    {----part c----}
+    writeln((22+10)/2);
+    {1.6000000000000000E+001 real}
+
+    {----part c----}
+    writeln(50*10-4 mod 3*5+80);
+    {575 integer}
+
+end.
