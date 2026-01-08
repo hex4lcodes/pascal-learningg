6e67522489617690adcb5037434141fdd4337975
Hex <hexasweetzmain@gmail.com>
2026-01-08 17:09:41 -0500

    Create priceofitem.pas
    this is the end of the chapter 1 files

---
+++ chapter-1/priceofitem.pas
@@ -0,0 +1,17 @@
+program PriceofanItem;
+
+var
+   price, saletax, total :real;
+
+begin
+    writeln('How much was the item: ');
+    readln(price);
+
+    saletax := price * 0.08;
+    total := price * saletax;
+
+    writeln('Subtotal:    ', price :0:2);
+    writeln('-----------------');
+    writeln('Sales Tax:  ', saletax :0:2);
+    writeln('Total:    ', total :0:2);
+end.
