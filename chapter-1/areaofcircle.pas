ac294fb630a5923f6f874ee380c78af8458469e7
Hex <hexasweetzmain@gmail.com>
2026-01-08 16:12:33 -0500

    Create areaofcircle.pas
    question 6 of the chapter 1 review

---
+++ chapter-1/areaofcircle.pas
@@ -0,0 +1,14 @@
+program AreaofCircle;
+
+var
+   pi,area,radius :real;
+
+begin
+    writeln('Enter the radius of the circle');
+    readln(radius);
+
+    pi := 3.14159265;
+    area := pi * sqr(radius);
+
+    writeln('The area of a circle of the given radius is: ', area :0:4);
+end.
