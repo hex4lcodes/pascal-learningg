c53aeb69b667566de02bf028a3d2adcdb90b63e6
Hex <hexasweetzmain@gmail.com>
2026-01-14 02:16:15 -0500

    Add files via upload

---
+++ chapter-2/hypotenuse.pas
@@ -0,0 +1,20 @@
+
+program Hypotenuse;
+
+var
+a,b,c :real;
+
+begin
+    writeln('Enter the value of the shorter leg (a)');
+    readln(a);
+
+    writeln('Enter the value of the longer leg (b)');
+    readln(b);
+
+    c := sqrt((sqr(a) + sqr(b)));
+
+    writeln('A: ', a :0:2);
+    writeln('B: ', b :0:2);
+
+    writeln('The hypotenuse given a and b is: ', c :0:2);
+end.
